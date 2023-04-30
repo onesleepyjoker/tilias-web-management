6195c80c763f8492e61c89a57eb8760b889e6501
lyf <2638479835@qq.com>
2023-04-30 15:00:24 +0800

    second commit

--- src/main/java/com/example/tiliaswebmanagement/utils/hello_git.java
+++ src/main/java/com/example/tiliaswebmanagement/utils/hello_git.java
@@ -3,5 +3,6 @@ package com.example.tiliaswebmanagement.utils;
 public class hello_git {
     public static void main(String[] args) {
         System.out.println("hello.git");
+        System.out.println("hello.git");
     }
 }
