47a128585eed37593f23408252fe633ee57376b6
lyf <2638479835@qq.com>
2023-04-30 15:11:56 +0800

    1 commit

--- src/main/java/com/example/tiliaswebmanagement/utils/hello_git.java
+++ src/main/java/com/example/tiliaswebmanagement/utils/hello_git.java
@@ -2,6 +2,10 @@ package com.example.tiliaswebmanagement.utils;
 
 public class hello_git {
     public static void main(String[] args) {
-
+        System.out.println("12321313");
+        System.out.println("12321313");
+        System.out.println("12321313");
+        System.out.println("12321313");
+        System.out.println("12321313");
     }
 }
